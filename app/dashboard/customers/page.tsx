77aa33a6e7f1db8a57996756e0a9c3e3f5eaca29
Aditya Rawat <125037123+adityawillcode@users.noreply.github.com>
2023-12-04 15:50:56 +0545

    next app basics

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>this is customer page</div>
+  )
+}
+
+export default page
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+
+function page () {
+  return (
+    <div>
+    this is dashboard page
+    </div>
+  )
+}
+
+export default page
